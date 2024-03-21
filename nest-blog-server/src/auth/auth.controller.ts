e2ad298b4a68602759fffe269c53c0ccc1f3e35c
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-03-21 15:05:07 +0700

    Add local and jwt passport strategy

--- nest-blog-server/src/auth/auth.controller.ts
+++ nest-blog-server/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { Public } from 'src/decorators/public';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req) {
+    return this.authService.login(req.user);
+  }
 }
--- nest-blog-server/src/auth/auth.service.ts
+++ nest-blog-server/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === password) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user?.username, sub: user?.userId };
+    console.log(payload);
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
