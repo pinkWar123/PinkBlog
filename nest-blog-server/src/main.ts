24681ad1dd282a32310618f7d2a13722ade61f17
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-03-25 16:00:53 +0700

    Add login logic

--- nest-blog-server/src/main.ts
+++ nest-blog-server/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog document')
+    .setDescription('This is API documentation for Blog server')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8000);
-  console.log('success');
 }
 bootstrap();
